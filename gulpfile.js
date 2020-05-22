2915bf26e470e17eacdf025200c26c0d76f1af95
Sjors <sjorspieter@live.nl>
2020-05-22 10:32:01 +0200

    updated gulpfile for better compiling and watching files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-  const gulp = require('gulp')
-  const requireDir = require('require-dir')('build-scripts')
+const gulp = require('gulp')
+const requireDir = require('require-dir')('build-scripts')
 
-  const sass = 'src/scss/**/*.scss'
-  const js = 'src/js/**/*.js'
+const sass = 'src/scss/**'
+const js = 'src/js/**'
 
-  gulp.task('default', gulp.parallel('sass', 'js'))
+gulp.task('default', gulp.parallel('sass', 'js'))
 
-  gulp.task('watch', () => {
-      gulp.watch(sass, gulp.series('sass'))
-      gulp.watch(js, gulp.series('js'))
-  })
+gulp.task('watch', () => {
+  gulp.watch(sass, gulp.series('sass'))
+  gulp.watch(js, gulp.series('js'))
+})
